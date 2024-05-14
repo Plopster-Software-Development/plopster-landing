7e44bdb1cd650e2461b3c4c7e74bff8bcbaf5279
Nicodav28 <esteveznicolas0@gmail.com>
2024-05-14 16:37:37 -0500

    adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-// import Projects from "./components/Projects";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App = () => {
   return (
     <>
+      <SpeedInsights />
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
