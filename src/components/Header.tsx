b54e83950d7df18174acb0eef96b0024d78f5ec6
Nicodav28 <esteveznicolas0@gmail.com>
2024-05-14 16:24:37 -0500

    adjustments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div
       className={`fixed top-0 left-0 w-full z-50 
-      border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm bg-n-8`}
+      border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm bg-n-8/90 backdrop-blur-sm`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 p-[1em]">
         <a className="block w-[12-rem] xl:mr-8" href="#hero">
